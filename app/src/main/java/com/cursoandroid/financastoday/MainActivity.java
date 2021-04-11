ab2d61bc44ce7d381e5e4c1955fd4dec7b225284
Iann Leles <iannleles@outlook.com>
2021-04-11 16:32:10 -0300

    Ajustando Slider

--- app/src/main/java/com/cursoandroid/financastoday/MainActivity.java
+++ app/src/main/java/com/cursoandroid/financastoday/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
+                .canGoForward(false)
                 .build());
     }
 }
