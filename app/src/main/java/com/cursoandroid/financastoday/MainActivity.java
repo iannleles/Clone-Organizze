fd813380bb07a49aa30402fd515f01205ca92c60
Iann Leles <iannleles@outlook.com>
2021-04-11 20:19:26 -0300

    Interface do cadastro de login

--- app/src/main/java/com/cursoandroid/financastoday/MainActivity.java
+++ app/src/main/java/com/cursoandroid/financastoday/MainActivity.java
@@ -2,8 +2,12 @@ package com.cursoandroid.financastoday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.cursoandroid.financastoday.activity.CadastroActivity;
+import com.cursoandroid.financastoday.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -32,7 +36,18 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
                 .canGoForward(false)
                 .build());
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
